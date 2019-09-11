0c7e55a8b33800997e83c12b20034acbf4e535dc
JackieLin <jackie@JackieLindeMacBook-Pro.local>
2019-09-11 13:14:47 +0800

    引用

--- app/src/main/java/com/xykj/javaskilldemo/java/TestFinal.java
+++ app/src/main/java/com/xykj/javaskilldemo/java/TestFinal.java
@@ -4,6 +4,9 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -14,19 +17,46 @@ public class TestFinal {
     public static void main(String[] args){
 
         //finally总是执行，除非程序或线程被中断，如果try里面一直死循环，那么finally也可能不执行
-        try{
-            System.out.println("======try=====");
-            System.exit(1);
-        }finally {
-            System.out.println("======finally=====");
-        }
+//        try{
+//            System.out.println("======try=====");
+//            System.exit(1);
+//        }finally {
+//            System.out.println("======finally=====");
+//        }
+//
+//        //List.of 本身创建的就是不可变对象
+//
+//        List<Integer> list = null;
+//
+//        Objects.requireNonNull(list);
 
-        //List.of 本身创建的就是不可变对象
-
-        List<Integer> list = null;
-
-        Objects.requireNonNull(list);
+        testReference();
 
     }
+
+    //虚引用关联队列
+    public static void testReference(){
+
+        Object object = new Object();
+        ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+
+        PhantomReference<Object> phantomReference = new PhantomReference<>(object,referenceQueue);
+
+        object = null;
+
+        System.gc();
+
+        try {
+            Reference<Object> reference = (Reference<Object>) referenceQueue.remove(1000l);
+            if (reference != null){
+                System.out.println("==========remove=====");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
 }
